15a0fdd175dcd3254527bd6ad8806a5c317ea824
katelynarmstrong <katelynarmstrong6@gmail.com>
2022-02-13 10:33:15 -0600

    question prompts started

--- index.js
+++ index.js
@@ -1,6 +1,69 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username (Required)',
+            validate: githubInput => {
+              if (githubInput) {
+                return true;
+              } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address (Required)',
+            validate: emailInput => {
+              if (emailInput) {
+                return true;
+              } else {
+                console.log('Please enter your email address!');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project? (Required)',
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log('You need to enter a project name!');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
+              if (descriptionInput) {
+                return true;
+              } else {
+                console.log('You need to enter a project description!');
+                return false;
+              }
+            }
+          },
+    ])
+}
+
+//What kind of liscense should your project have?
+//What command should be run to install dependencies?
+//What command should be run to run tests?
+//What does the user need to know about using the repo?
+//What does the user need to know about contributing to the repo?
 const questions = [];
 
 // TODO: Create a function to write README file
